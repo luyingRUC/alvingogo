3452217f82df5f877c8ffbf791615ef9bf1797b8
Alvin LU Ying <luying92@ruc.edu.cn>
2021-08-23 20:51:30 +0800

    revised getMeanList() function

--- alvindebug.py
+++ alvindebug.py
@@ -1,6 +1,6 @@
 import os
 
-def setDebugDirt(directory):
+def setExctDirt(directory):
     dir_old = os.getcwd()
     try:
         os.chdir(directory)
--- alvinlist.py
+++ alvinlist.py
@@ -2,5 +2,12 @@ def listDeduplicate(inputList):
     # this function helps to deduplicate elements in the given input list
     return list(dict.fromkeys(inputList))
 
+from numpy import isnan, isinf
 def getMeanList(inputList):
-    return sum(inputList) / len(inputList)
+    list1 = []
+    for l in inputList: 
+        if((not isnan(l)) and (not isinf(l)) ):list1.append(l)
+    if( len(list1) ==0 ):
+        return 1.0
+    else: 
+        return float(sum(list1) / len(list1))
