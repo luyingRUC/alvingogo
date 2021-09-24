810ab85ca4ff4d9dd0af68ccc08b5c002e9d3182
Alvin LU Ying <alvinluying@gmail.com>
2021-09-25 02:11:23 +0800

    ddd

--- alvinnlp.py
+++ alvinnlp.py
@@ -1,5 +1,6 @@
 import nltk
-# nltk.download('wordnet')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 from nltk.tokenize import word_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
@@ -9,7 +10,7 @@ from nltk.corpus import wordnet
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 from nltk import pos_tag
 from stop_words import get_stop_words
-if("and" in get_stop_words("en")):    
+if("and" in get_stop_words("en")):
     get_stop_words("en").remove("and")
 
 import re
@@ -23,7 +24,6 @@ def generateCleanText(intputText, outputType):
 
     # Remove punctuations
     text = re.sub('[^a-zA-Z+]', ' ', intputText)
-
 
     # remove tags
     text = re.sub("&lt;/?.*?&gt;", " &lt;&gt; ", text)
@@ -225,9 +225,7 @@ def getCleanString_disco(intputText):
     ''' outputType = 
         1. list: return a list of clean words : like [Apple, has, power, compelling, to, Microsoft]
         2. string: return a string of clean words, each of which is joint by " ", like "Apple has power compelling to Microsoft"
-    '''
-
-    
+    '''   
 
     # replace all charaters except: letters, digit numbers, any of {+.-/}
     text = re.sub('[^a-zA-Z0-9+/.-]', ' ', intputText)
@@ -239,33 +237,31 @@ def getCleanString_disco(intputText):
     text = re.sub(cleanr, '', text)
 
     # clean and tokenize document string
-    tokenizer = RegexpTokenizer(r'[\w+/-]+')
+    tokenizer = RegexpTokenizer(r'[^,\s]+')
     tokens = tokenizer.tokenize(text)
 
-    # filter the POS tags, if the token is not a noun or adj, chop it out
-    # ok_tags = ['JJ', 'JJR', 'JJS','NN', 'NNS','NNP','NNPS','FW']
-    # ok_tags = ['NN', 'NNS','NNP','NNPS','FW', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
-    # word_tuples = pos_tag(tokens)
-    # tokens2 = []
-    # for _tuple in word_tuples:
-    #     if _tuple[1] in ok_tags:
-    #         tokens2.append(_tuple[0])
-
     # remove stop words from tokens
-    # create English stop words list
     en_stop = get_stop_words('en')
     tokens = [i for i in tokens if not i in en_stop]
 
-    # stem tokens
-    p_stemmer = PorterStemmer()
-    tokens_coded = [p_stemmer.stem(i) for i in tokens]
+    # only keep verb, nouns and adjectives 
+    word_tuples = pos_tag(tokens)
+    tokens2 = []
+    for _tuple in word_tuples:
+        # if _tuple[1].startswith(("N", "J", "V")):
+        if _tuple[1].startswith(("N")):
+            tokens2.append(_tuple[0])
+
+    # # stem tokens
+    # p_stemmer = PorterStemmer()
+    # tokens_coded = [p_stemmer.stem(i) for i in tokens2]
 
 
-    # # Lemmatization
-    # tokens_coded = []
-    # lemmatizer = WordNetLemmatizer()
-    # for word in tokens:
-    #     tokens_coded.append(lemmatizer.lemmatize(word))
+    # Lemmatization
+    tokens_coded = []
+    lemmatizer = WordNetLemmatizer()
+    for word in tokens:
+        tokens_coded.append(lemmatizer.lemmatize(word))
 
     # delete all single characters
     multiples = []
@@ -277,6 +273,3 @@ def getCleanString_disco(intputText):
 
     multiples = " ".join(multiples)
     return multiples
-
-# sent = "runs running, programs are ASP3.50, c++"
-# print(getCleanString_disco(sent, "string"))
